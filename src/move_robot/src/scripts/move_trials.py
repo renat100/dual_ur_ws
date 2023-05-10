d25b3b8fb6b2425ab318d699ca492d0390b60ae6
Renat Kermenov <r.kermenov@pm.univpm.it>
2023-05-10 12:52:00 +0200

    initial commit

---
+++ src/move_robot/src/scripts/move_trials.py
@@ -0,0 +1,72 @@
+#!/usr/bin/env python
+
+import copy
+import sys
+import rospy
+import moveit_commander
+import moveit_msgs.msg
+
+
+moveit_commander.roscpp_initialize(sys.argv)
+rospy.init_node("move_group_python_interface", anonymous=True)
+robot = moveit_commander.RobotCommander()
+scene = moveit_commander.PlanningSceneInterface()
+
+planning_group_arm = "left_arm"
+planning_group_gripper = "left_gripper"
+
+move_group = moveit_commander.MoveGroupCommander(planning_group_arm)
+
+display_trajectory_publisher = rospy.Publisher(
+    "/move_group/display_planned_path",
+    moveit_msgs.msg.DisplayTrajectory,
+    queue_size=20,
+)
+
+# We can get the name of the reference frame for this robot:
+planning_frame = move_group.get_planning_frame()
+print("============ Planning frame: %s" % planning_frame)
+
+# We can also print the name of the end-effector link for this group:
+eef_link = move_group.get_end_effector_link()
+print("============ End effector link: %s" % eef_link)
+
+# We can get a list of all the groups in the robot:
+group_names = robot.get_group_names()
+print("============ Available Planning Groups:", robot.get_group_names())
+
+# Sometimes for debugging it is useful to print the entire state of the
+# robot:
+print("============ Printing robot state")
+print(robot.get_current_state())
+print("")
+print(move_group.get_current_pose().pose)
+
+scale = 0.1
+waypoints = []
+
+wpose = move_group.get_current_pose().pose
+wpose.position.z -= scale * 0.1  # First move up (z)
+wpose.position.y += scale * 0.2  # and sideways (y)
+waypoints.append(copy.deepcopy(wpose))
+
+wpose.position.x += scale * 0.1  # Second move forward/backwards in (x)
+waypoints.append(copy.deepcopy(wpose))
+
+wpose.position.y -= scale * 0.1  # Third move sideways (y)
+waypoints.append(copy.deepcopy(wpose))
+
+# We want the Cartesian path to be interpolated at a resolution of 1 cm
+# which is why we will specify 0.01 as the eef_step in Cartesian
+# translation.  We will disable the jump threshold by setting it to 0.0,
+# ignoring the check for infeasible jumps in joint space, which is sufficient
+# for this tutorial.
+(plan, fraction) = move_group.compute_cartesian_path(
+    waypoints, 0.01, 0.0  # waypoints to follow  # eef_step
+)  # jump_threshold
+display_trajectory = moveit_msgs.msg.DisplayTrajectory()
+display_trajectory.trajectory_start = robot.get_current_state()
+display_trajectory.trajectory.append(plan)
+# Publish
+display_trajectory_publisher.publish(display_trajectory)
+move_group.execute(plan, wait=True)
